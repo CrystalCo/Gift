c6ad5fe0595b59439d130bfa5d2b0206cc92a2ca
Crystal C <crystal.contr@gmail.com>
2018-10-24 14:32:40 -0500

    installed Cypress, nodes, and testing on app_spec.js.  Updated package.json's scripts to include cypress

---
+++ project/frontend/cypress/integration/app_spec.js
@@ -0,0 +1,28 @@
+describe("Django REST framework / React quickstart app", () => {
+    const profile = {
+        name: "Armin",
+        email: "some-email@gmail.com",
+        message: "I am looking for a React tutor"
+    };
+    before(() => {
+        cy.exec("npm run dev");
+        cy.exec("npm run flush");
+    });
+    it("should be able to fill a web form", () => {
+        cy.visit("/");
+        cy
+            .get('input[name="name"]')
+            .type(profile.name)
+            .should("have.value", profile.name);
+        cy
+            .get('input[name="email"]')
+            .type(profile.email)
+            .should("have.value", profile.email);
+        cy
+            .get('textarea[name="measurements"]')
+            .type(profile.measurements)
+            .should("have.value", profile.measurements);
+        cy.get("form").submit();
+    });
+    // more tests here
+  });
