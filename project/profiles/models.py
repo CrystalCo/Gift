d6af0c4c2ddef029d7179f0c6942180405fec655
Crystal C <crystal.contr@gmail.com>
2018-10-24 09:28:06 -0500

    installed React components, nodes, package.json, babel, & webpack 4 config

--- project/profiles/models.py
+++ project/profiles/models.py
@@ -3,5 +3,5 @@ from django.db import models
 class Profile(models.Model):
     name = models.CharField(max_length=100)
     email = models.EmailField()
-    measurements = models.CharField(max_length=15)
+    measurements = models.CharField(max_length=20)
     created_at = models.DateTimeField(auto_now_add=True)
