14f05ee5f0d098e85c34dd5beec63140c510d8b1
Crystal C <crystal.contr@gmail.com>
2018-11-01 04:07:57 -0500

    created ListItem model and migrated it.

--- project/lists/models.py
+++ project/lists/models.py
@@ -1,3 +1,9 @@
 from django.db import models
+from django.utils import timezone
 
 # Create your models here.
+
+class ListItem(models.Model):
+    productName = models.CharField(max_length=200)
+    price = models.DecimalField(max_digits=12, decimal_places=2)
+    brand = models.CharField(max_length=100)
