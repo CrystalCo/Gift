27a9825b6aea6f5b6f4411c8216d745c032b79c9
Crystal C <crystal.contr@gmail.com>
2018-11-06 00:27:55 -0600

    Added wishlists views and serializers.

--- project/wishlists/views.py
+++ project/wishlists/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from wishlists.serializers import WishSerializer
+from rest_framework import generics
 
 # Create your views here.
+class WishListCreate(generics.ListCreateAPIView):
+    queryset = Wish.objects.all()
+    serializer_class = WishSerializer
