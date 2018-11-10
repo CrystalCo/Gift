35a75f4ece651119f9165d286f1d48d6a0494ae6
Crystal C <crystal.contr@gmail.com>
2018-11-09 20:53:46 -0600

    Trying to create a todo list/gift list in Django/React.

--- project/frontend/src/components/App/App.js
+++ project/frontend/src/components/App/App.js
@@ -1,24 +1,108 @@
 import React from "react";
-import ReactDOM from "react-dom";
+import './App.css';
+
+// import ReactDOM from "react-dom";
 import DataProvider from "../DataProvider/DataProvider";
 import Table from "../Table/Table";
 import Form from "../Form/Form";
+import GiftList from "../GiftList/GiftList";
 import WishListForm from "../WishListForm/WishListForm";
 
-const App = () => (
-    <React.Fragment>
-        <DataProvider endpoint="api/profile/" 
-            render={data => <Table data={data} />} 
-        />
-        <Form endpoint="api/profile/" />
+class App extends React.Component {
+    constructor(props) {
+        super(props);
+    
+        this.state = {
+          items: [],
+          currentItem: {text:'', key:''},
+        };
+    
+        this.handleInput = this.handleInput.bind(this);
+        this.addItem = this.addItem.bind(this);
+    }
 
-        <DataProvider endpoint="api/wish/" 
-            render={data => <Table data={data} />} 
-        />
-        <WishListForm endpoint="api/wish/" />
-    </React.Fragment>
-);
+    handleInput = e => {
+        const itemText = e.target.value;
+        const currentItem = {
+            text: itemText,
+            key: Date.now()
+        };
+        this.setState({
+            currentItem,
+        });
+    }
 
-const wrapper = document.getElementById("app");
+    addItem = e => {
+        e.preventDefault();
+        const newItem = this.state.currentItem;
+        if (newItem !== '') {
+            console.log(newItem);
+            const items = [...this.state.items, newItem];
+            this.setState({
+                items: items,
+                currentItem: { 
+                    text: '',
+                    key: ''
+                }
+            });
+        }
+    }
 
-wrapper ? ReactDOM.render(<App />, wrapper) : null;
+    render() {
+        return (
+            <div className="App">
+                <h1>Gift App</h1>
+                <div className="App">
+                    <div className="GiftList">
+                        <GiftList 
+                            addItem={this.addItem} 
+                            inputElement={this.inputElement} 
+                            handleInput={this.handleInput}
+                            currentItem={this.state.currentItem} 
+                        />
+                    </div>
+                    <div className="Profile">
+                        <DataProvider endpoint="api/profile/" 
+                            render={data => <Table data={data} />} 
+                        />
+                        <Form endpoint="api/profile/" />
+                    </div>
+
+                    <div className="Wish">
+                        <DataProvider endpoint="api/wish/" 
+                            render={data => <Table data={data} />} 
+                        />
+                        <WishListForm endpoint="api/wish/" />
+                    </div>
+                </div>
+            </div>
+        );
+    }
+}
+
+export default App;
+
+// const App = () => (
+//     <React.Fragment>
+//         <h1>Gift App</h1>
+//         <div className="App">
+//             <div className="Profile">
+//                 <DataProvider endpoint="api/profile/" 
+//                     render={data => <Table data={data} />} 
+//                 />
+//                 <Form endpoint="api/profile/" />
+//             </div>
+
+//             <div className="Wish">
+//                 <DataProvider endpoint="api/wish/" 
+//                     render={data => <Table data={data} />} 
+//                 />
+//                 <WishListForm endpoint="api/wish/" />
+//             </div>
+//         </div>
+//     </React.Fragment>
+// );
+
+// const wrapper = document.getElementById("app");
+
+// wrapper ? ReactDOM.render(<App />, wrapper) : null;
--- project/project/urls.py
+++ project/project/urls.py
@@ -15,10 +15,12 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from todo_list import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('profiles.urls')),
     path('', include('frontend.urls')),
     path('', include('wishlists.urls')),
+    path('', include('todo_list.urls')),
 ]
